6c637323cbf3684f2d980eab94132fc0109edfce
Daniel Meechan <daniel.meechan.1@gmail.com>
2019-11-12 22:34:41 +0000

    Display DB data in UI

--- lib/ui/sleep_logs.dart
+++ lib/ui/sleep_logs.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:sleepyroom/models/sleep_log.dart';
+import 'package:sleepyroom/utils/timestamps.dart';
 
 class SleepLogs extends StatefulWidget {
   SleepLogs({Key key, this.title}) : super(key: key);
@@ -19,27 +21,35 @@ class _SleepLogsState extends State<SleepLogs> {
 
   /// Show list of every sleep log recorded: date, light score, sound score
   Widget _buildSleepLogs() {
-    return ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        itemBuilder: (context, i) {
-        if (i.isOdd) return Divider();
+    return FutureBuilder<List<SleepLog>>(
+        future: SleepLog.getAll(),
+        builder:
+            (BuildContext context, AsyncSnapshot<List<SleepLog>> snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+                padding: const EdgeInsets.all(16.0),
+                itemCount:
+                    snapshot.data.length * 2 - 1, // add space for dividers
+                itemBuilder: (context, i) {
+                  if (i.isOdd) return Divider();
 
-        final index = i ~/ 2;
-        if (index >= 10) {
-          // fetch 10 more from the DB??
-        }
-
-        return _buildRow(10);
-    });
+                  final index = i ~/ 2;
+                  return _buildRow(snapshot.data[index]);
+                });
+          } else {
+            return Center(child: CircularProgressIndicator());
+          }
+        });
   }
 
-  Widget _buildRow(int index) {
-    return ListTile(
-      title: Text("1st Jan 2019"),
-      onTap: () => setState(() => {
+  Widget _buildRow(SleepLog sleepLog) {
+    final start = toWeekday(sleepLog.startDate.weekday) + " at " + sleepLog.startDate.hour.toString() + ":" + sleepLog.startDate.minute.toString();
 
-      })
-    );
+    return ListTile(
+        title: Text(start),
+        trailing: Text(sleepLog.noiseScore.toString() +
+            " | " +
+            sleepLog.lightScore.toString()));
   }
 
   Widget build(BuildContext context) {
--- lib/utils/timestamps.dart
+++ lib/utils/timestamps.dart
@@ -5,3 +5,24 @@ DateTime fromEpoch(int epoch) {
 int toEpoch(DateTime dateTime) {
   return dateTime != null ? dateTime.millisecondsSinceEpoch : null;
 }
+
+String toWeekday(int day) {
+  switch (day) {
+    case 1:
+      return "Monday";
+    case 2:
+      return "Tuesday";
+    case 3:
+      return "Wednesday";
+    case 4:
+      return "Thursday";
+    case 5:
+      return "Friday";
+    case 6:
+      return "Saturday";
+    case 7:
+      return "Sunday";
+    default:
+      return null;
+  }
+}
